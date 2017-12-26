0d0adc0ffb536cc69417fd4e013e25c5ad96cb5c
Matsuda Kenichi <blackdarkrose91@gmail.com>
2017-12-26 13:06:24 +0800

    Player Unit AI and FSM (Working on unit with harvest AI)

--- Assets/Scripts/Enemy/EnemyBehaviour.cs
+++ Assets/Scripts/Enemy/EnemyBehaviour.cs
@@ -8,26 +8,42 @@ public class EnemyBehaviour : MonoBehaviour {
            EUS_GUARD,
            EUS_ATTACK,
            EUS_MOVE,
+           EUS_HARVEST,
            EUS_MAX_STATES
     };
 
     public float f_health = 50;
-    public float f_range = 0.1f;
+    public float f_range = 0.5f;
     public float f_atkRange = 0.02f;
     public float f_speed = 0.01f;
-    private GameObject go_playerList;
-    private GameObject[] goList_playerList;
+    private Transform T_playerList;
+    //private GameObject[] goList_playerList;
     private GameObject go_LockOnPlayerUnit;
     EnemyUnitState EUS;
 
-	// Use this for initialization
-	void Start () {
-        go_playerList = GameObject.FindGameObjectWithTag("PlayerUnit");
-        EUS = EnemyUnitState.EUS_GUARD; ;
+    RaycastHit rcHit;
+    Vector3 rcHitPosition;
+    private float f_distanceY;
+    private Vector3 offset_Y;
+
+
+
+    // Use this for initialization
+    void Start () {
+        T_playerList = GameObject.FindGameObjectWithTag("PlayerList").transform;
+        EUS = EnemyUnitState.EUS_GUARD;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+        CheckWhetherStillOnGround();
+        if (Physics.Raycast(gameObject.GetComponent<Transform>().transform.position, Vector3.down, out rcHit))
+        {
+            rcHitPosition = rcHit.point;
+            f_distanceY = gameObject.GetComponent<Transform>().transform.position.y - rcHitPosition.y;
+            offset_Y = new Vector3(0, f_distanceY, 0);
+        }
 
         switch (EUS)
         {
@@ -35,11 +51,16 @@ public class EnemyBehaviour : MonoBehaviour {
                 break;
 
             case EnemyUnitState.EUS_ATTACK:
-                break;
+                {
+                    AttackPlayerUnit();
+                    break;
+                }
 
             case EnemyUnitState.EUS_GUARD:
-                DetectPlayerUnit();
-                break;
+                {
+                    DetectPlayerUnit();
+                    break;
+                }
         }
 
         DeathCheck();
@@ -55,26 +76,38 @@ public class EnemyBehaviour : MonoBehaviour {
 
     void DetectPlayerUnit()
     {
-        foreach (Transform go_playerUnitChild in go_playerList.transform)
+        foreach (Transform go_playerUnitChild in T_playerList)
         {
-            float f_distanceCheck = (go_playerUnitChild.transform.position - gameObject.transform.position).sqrMagnitude;
-            if (f_distanceCheck < f_range)
+            go_LockOnPlayerUnit = go_playerUnitChild.gameObject;
+            float f_distanceCheck = (go_playerUnitChild.position - gameObject.GetComponent<Transform>().position).sqrMagnitude;
+            if (f_distanceCheck < f_range*f_range)
             {
-                go_LockOnPlayerUnit = go_playerUnitChild.gameObject;
-                gameObject.transform.LookAt(go_LockOnPlayerUnit.transform.position);
                 EUS = EnemyUnitState.EUS_ATTACK;
-            }
-            else
-            {
-                EUS = EnemyUnitState.EUS_GUARD;
+                Debug.Log("Enemy Saw U");
             }
         }
     }
 
     void AttackPlayerUnit()
     {
-        float f_distanceCheck = (go_LockOnPlayerUnit.transform.position - gameObject.transform.position).sqrMagnitude;
-        if (f_distanceCheck >= f_atkRange)
-            gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, go_LockOnPlayerUnit.transform.position, f_speed * Time.deltaTime);
+        gameObject.GetComponent<Transform>().LookAt(go_LockOnPlayerUnit.GetComponent<Transform>().position);
+        float f_distanceCheck = (go_LockOnPlayerUnit.GetComponent<Transform>().position - gameObject.GetComponent<Transform>().position).sqrMagnitude;
+        if (f_distanceCheck >= f_atkRange && f_distanceCheck <= f_range*f_range)
+            gameObject.GetComponent<Transform>().position = Vector3.MoveTowards(gameObject.GetComponent<Transform>().position, go_LockOnPlayerUnit.GetComponent<Transform>().position, f_speed * Time.deltaTime);
+        else if (f_distanceCheck > f_range*f_range)
+        {
+            Debug.Log("ENemy lost sight of u");
+            EUS = EnemyUnitState.EUS_GUARD;
+        }
+    }
+
+    void CheckWhetherStillOnGround()
+    {
+        if ((gameObject.GetComponent<Transform>().transform.position.y - rcHitPosition.y) != f_distanceY)
+        {
+            gameObject.GetComponent<Transform>().transform.position.Set(gameObject.GetComponent<Transform>().transform.position.x,
+                                                                        rcHitPosition.y + f_distanceY,
+                                                                        gameObject.GetComponent<Transform>().transform.position.z);
+        }
     }
 }
--- Assets/Scripts/Player/PlayerUnitBehaviour.cs
+++ Assets/Scripts/Player/PlayerUnitBehaviour.cs
@@ -4,9 +4,22 @@ using UnityEngine;
 
 public class PlayerUnitBehaviour : MonoBehaviour
 {
+    enum PlayerUnitState
+    {
+        PUS_GUARD,
+        PUS_ATTACK,
+        PUS_MOVE,
+        PUS_HARVEST,
+        PUS_MAX_STATES
+    }
+    PlayerUnitState PUS;
 
     private Transform T_Enemy;
     public Color detectedColor;
+    private GameObject go_GoldMine;
+    private GameObject go_Depot;
+
+    bool b_StartHarvest;
 
     // Use this for initialization
     void Start()
@@ -18,17 +31,66 @@ public class PlayerUnitBehaviour : MonoBehaviour
     void Update()
     {
 
+        switch (PUS)
+        {
+            case PlayerUnitState.PUS_MOVE:
+                break;
+
+            case PlayerUnitState.PUS_ATTACK:
+                {
+                    break;
+                }
+
+            case PlayerUnitState.PUS_GUARD:
+                {
+                    DetectEnemyUnit();
+                    break;
+                }
+            case PlayerUnitState.PUS_HARVEST:
+                {
+                    OnHarvestMode();
+                    break;
+                }
+        }
+
+      
+    }
+
+    void DetectEnemyUnit()
+    {
         foreach (Transform T_enemyChild in T_Enemy)
         {
-            if ((T_enemyChild.position - gameObject.GetComponent<Transform>().position).sqrMagnitude <= gameObject.GetComponent<PlayerUnitUpdate>().GetRange())
+            if ((T_enemyChild.position - gameObject.GetComponent<Transform>().position).sqrMagnitude <= gameObject.GetComponent<PlayerUnitUpdate>().GetRange() * gameObject.GetComponent<PlayerUnitUpdate>().GetRange())
             {
                 //DO something
-                gameObject.GetComponent<Renderer>().material.color = detectedColor;
+                PUS = PlayerUnitState.PUS_ATTACK;
+                gameObject.GetComponent<Renderer>().material.color = Color.green;
+                Debug.Log("Detected");
             }
             else
             {
                 gameObject.GetComponent<Renderer>().material.color = gameObject.GetComponent<PlayerUnitUpdate>().GetDefault();
             }
+
+        }
+    }
+
+    void OnHarvestMode()
+    {
+        go_GoldMine = GameObject.FindGameObjectWithTag("Resources");
+        go_Depot = GameObject.FindGameObjectWithTag("BuildingList");
+        foreach (Transform go_PlayerBuilding in go_Depot.transform)
+        {
+            if (go_PlayerBuilding.gameObject.name == "ResourceDepot")
+            {
+                go_Depot = go_PlayerBuilding.gameObject;
+                b_StartHarvest = true;
+                break;
+            }
+        }
+
+        if (b_StartHarvest)
+        {
 
         }
     }
