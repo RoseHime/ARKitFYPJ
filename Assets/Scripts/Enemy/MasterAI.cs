6c180b3f19c5d0d6cf40fa713c6a1d05b13f432c
Keith <keith_4.2@hotmail.com>
2018-01-03 10:50:25 +0800

    Updated BuildingInfo and MasteAI slightly

--- Assets/Scripts/Enemy/MasterAI.cs
+++ Assets/Scripts/Enemy/MasterAI.cs
@@ -8,6 +8,7 @@ public class MasterAI : MonoBehaviour {
     {
         PINCER,
         TURTLE,
+        DIRECT,
     }
 
     public STRATEGY masterStrategy;
@@ -16,12 +17,18 @@ public class MasterAI : MonoBehaviour {
 
     bool startStrategy = false;
 
-    private int i_WaypointCount;
+    private int i_PincerWaypointCount;
+    private int i_TurtleWaypointCount;
+    private int i_DirectWaypointCount;
     private int i_CurrentWaypoint = 0;
+
+    bool b_IsDefending = false;
 
 	// Use this for initialization
 	void Start () {
-        i_WaypointCount = transform.GetChild(0).childCount;
+        i_PincerWaypointCount = transform.GetChild(0).GetChild(0).childCount;
+        i_TurtleWaypointCount = transform.GetChild(1).GetChild(0).childCount;
+        i_DirectWaypointCount = transform.GetChild(2).GetChild(0).childCount;
     }
 	
 	// Update is called once per frame
@@ -32,6 +39,10 @@ public class MasterAI : MonoBehaviour {
                 PincerStrat();
                 break;
             case STRATEGY.TURTLE:
+                TurtleStrat();
+                break;
+            case STRATEGY.DIRECT:
+                DirectStrat();
                 break;
         }	
 	}
@@ -59,13 +70,13 @@ public class MasterAI : MonoBehaviour {
         }
         else
         {
-            if (!CheckIfUnitsMoving() && i_CurrentWaypoint < i_WaypointCount)
+            if (!CheckIfUnitsMoving() && i_CurrentWaypoint < i_PincerWaypointCount)
             {
                 for (int i = 0; i < spawnerList.Count; i++)
                 {
                     if (!spawnerList[i].isRetreating)
                     {
-                        spawnerList[i].MoveUnits(transform.GetChild(i).GetChild(i_CurrentWaypoint).position);
+                        spawnerList[i].MoveUnits(transform.GetChild(0).GetChild(i).GetChild(i_CurrentWaypoint).position);
                     }                    
                 }
                 i_CurrentWaypoint++;
@@ -108,5 +119,118 @@ public class MasterAI : MonoBehaviour {
             }
         }
         return false;
+    }
+
+    void TurtleStrat()
+    {
+        if (!startStrategy)
+        {
+            bool checkIfAllSpawned = true;
+            for (int i = 0; i < spawnerList.Count; ++i)
+            {
+                if (spawnerList[i].localEnemyList.Count != spawnerList[i].i_maxUnits)
+                {
+                    checkIfAllSpawned = false;
+                }
+            }
+            if (checkIfAllSpawned)
+            {
+                startStrategy = true;
+                for (int i = 0; i < spawnerList.Count; i++)
+                {
+                    spawnerList[i].isRetreating = false;
+                }
+            }
+        }
+        else
+        {
+            if (!CheckIfUnitsMoving() && i_CurrentWaypoint < i_TurtleWaypointCount)
+            {
+                for (int i = 0; i < spawnerList.Count; i++)
+                {
+                    if (!spawnerList[i].isRetreating)
+                    {
+                        spawnerList[i].MoveUnits(transform.GetChild(1).GetChild(i).GetChild(i_CurrentWaypoint).position);
+                    }
+                }
+                i_CurrentWaypoint++;
+            }
+            for (int i = 0; i < spawnerList.Count; i++)
+            {
+                if (spawnerList[i].f_morale <= 20 && !spawnerList[i].isRetreating)
+                {
+                    spawnerList[i].RetreatUnits(spawnerList[i].transform.GetChild(0).position);
+                }
+            }
+
+            foreach (Transform building in GameObject.FindGameObjectWithTag("EnemyBuildingList").transform)
+            {
+                BuildingInfo buildInfo = building.GetComponent<BuildingInfo>();
+                if (buildInfo.b_IsUnderAttack)
+                {
+                    b_IsDefending = true;
+                    for (int i = 0; i < spawnerList.Count; i++)
+                    {
+                        spawnerList[i].MoveUnits(buildInfo.transform.GetChild(0).position);
+                    }
+                }
+            }
+
+            if (!CheckIfUnitsAlive() && !b_IsDefending)
+            {
+                startStrategy = false;
+                i_CurrentWaypoint = 0;
+            }
+        }
+    }
+
+    void DirectStrat()
+    {
+        if (!startStrategy)
+        {
+            bool checkIfAllSpawned = true;
+            for (int i = 0; i < spawnerList.Count; ++i)
+            {
+                if (spawnerList[i].localEnemyList.Count != spawnerList[i].i_maxUnits)
+                {
+                    checkIfAllSpawned = false;
+                }
+            }
+            if (checkIfAllSpawned)
+            {
+                startStrategy = true;
+                for (int i = 0; i < spawnerList.Count; i++)
+                {
+                    spawnerList[i].isRetreating = false;
+                }
+            }
+        }
+        else
+        {
+            if (!CheckIfUnitsMoving() && i_CurrentWaypoint < i_DirectWaypointCount)
+            {
+                for (int i = 0; i < spawnerList.Count; i++)
+                {
+                    if (!spawnerList[i].isRetreating)
+                    {
+                        spawnerList[i].MoveUnits(transform.GetChild(2).GetChild(i).GetChild(i_CurrentWaypoint).position);
+                    }
+                }
+                i_CurrentWaypoint++;
+            }
+            for (int i = 0; i < spawnerList.Count; i++)
+            {
+                if (spawnerList[i].f_morale <= 20 && !spawnerList[i].isRetreating)
+                {
+                    spawnerList[i].RetreatUnits(spawnerList[i].transform.GetChild(0).position);
+                }
+            }
+
+            if (!CheckIfUnitsAlive()  && !b_IsDefending)
+            {
+                startStrategy = false;
+                i_CurrentWaypoint = 0;
+            }
+        }
     }
 }
