544fb1692114db9c30e54806a694fc87e189e6cd
Matsuda Kenichi <blackdarkrose91@gmail.com>
2018-01-05 10:34:12 +0800

    Trying to fix touch input

--- Assets/Scripts/TouchInput.cs
+++ Assets/Scripts/TouchInput.cs
@@ -21,7 +21,6 @@ public class TouchInput : MonoBehaviour {
 
     public bool b_BuildTower;
 
-    private GameObject go_AllTree;
 
     private void Start()
     {
@@ -67,22 +66,18 @@ public class TouchInput : MonoBehaviour {
                     {
                         go_PlayerUnit = recipient;
                         go_PlayerUnit.GetComponent<PlayerUnitBehaviour>().b_Selected = true;
+                        go_PlayerUnit.SendMessage("OnClick", hit.point, SendMessageOptions.DontRequireReceiver);
                         b_SomethingIsSelected = true;
                     }
                     else if (recipient.tag == "SelectableBuilding")
                     {
                         // I still havn't written anything here, probably will soon
-                        //go_PlayerBuilding = recipient;
-                        //go_PlayerBuilding.GetComponent<BuildingInfo>().b_Selected = true;
+                        go_PlayerBuilding = recipient;
+                        go_PlayerBuilding.SendMessage("OnClick", hit.point, SendMessageOptions.DontRequireReceiver);
                     }
                     else
                     {
 
-                    }
-
-                    if (Input.GetTouch(0).phase == TouchPhase.Began)
-                    {
-                        recipient.SendMessage("OnClick", hit.point, SendMessageOptions.DontRequireReceiver);
                     }
                 }
             }
@@ -233,15 +228,6 @@ public class TouchInput : MonoBehaviour {
         if (go_PlayerUnit.GetComponent<PlayerUnitBehaviour>().b_Selected)
         {
             b_StopRun = true;
-        }
-    }
-
-    public void FixedUpdate()
-    {
-        foreach (Transform GetTree in GameObject.FindGameObjectWithTag("Resources").transform)
-        {
-            if (GetTree.tag == "Tree")
-                go_AllTree = GetTree.gameObject;
         }
     }
 }
