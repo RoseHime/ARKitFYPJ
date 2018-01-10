210784e9eb16ea08325c23ea75377f9466c22781
Matsuda Kenichi <blackdarkrose91@gmail.com>
2018-01-10 11:52:47 +0800

    Change crosshair for detection check

---
+++ Assets/ChangeCrossHair.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeCrossHair : MonoBehaviour
+{
+
+    public GameObject go_Crosshair;
+    private RaycastHit hit;
+    public LayerMask touchInputMask;
+    public Texture2D getImage;
+    public Texture2D origin;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray;
+        ray = Camera.main.ScreenPointToRay(transform.position);
+        if (Physics.Raycast(ray, out hit, float.MaxValue, touchInputMask))
+        {
+            GameObject recipient = hit.transform.gameObject;
+
+            //debugText.text = recipient.name + "\n" + hit.point;
+            if (recipient != null)
+            {
+                if (recipient.tag == "PlayerUnit")
+                    this.GetComponent<RawImage>().texture = getImage;
+                else
+                    this.GetComponent<RawImage>().texture = origin;
+            }
+        }
+    }
+}
