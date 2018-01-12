15c68a59002e4e81d4694263da0b41d3374084da
Keith <keith_4.2@hotmail.com>
2018-01-12 09:54:22 +0800

    Added LOD script as well as moved some scripts to the Misc folder

---
+++ Assets/Scripts/Misc/LODScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LODScript : MonoBehaviour {
+
+    public Mesh LowQuality = null;
+    public Mesh MedQuality = null;
+    public Mesh HighQuality = null;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float distance = (Camera.main.transform.position - transform.position).magnitude;
+        PlayerInfo playerInfo = GameObject.FindGameObjectWithTag("PlayerInfo").GetComponent<PlayerInfo>();
+        if (distance < playerInfo.f_LODHighQuality)
+        {
+            GetComponent<MeshFilter>().mesh = HighQuality;
+        }
+        else if (distance < playerInfo.f_LODMedQuality)
+        {
+            GetComponent<MeshFilter>().mesh = MedQuality;
+        }
+        else
+        {
+            GetComponent<MeshFilter>().mesh = LowQuality;
+        }
+	}
+}
