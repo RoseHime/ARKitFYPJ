50bc5a29df388b722c16ca84cfe7b7ed2edd3c57
Matsuda Kenichi <blackdarkrose91@gmail.com>
2018-01-23 19:08:31 +0800

    able to adjust volume during gamerplay

---
+++ Assets/Audio.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Audio : MonoBehaviour {
+
+    AudioSource audio;
+    public AudioClip clip1;
+    public AudioClip clip2;
+
+    private PlayerInfo pi;
+    private bool b_NextSong;
+
+	// Use this for initialization
+	void Start () {
+        pi = GameObject.FindGameObjectWithTag("PlayerInfo").GetComponent<PlayerInfo>();
+
+        b_NextSong = false;
+        audio = GetComponent<AudioSource>();
+        audio.clip = clip1;
+        audio.Play();
+
+        DontDestroyOnLoad(this);
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (!b_NextSong && pi.i_playerLevel == 2)
+        {
+            b_NextSong = true;
+            audio.Stop();
+            audio.clip = clip2;
+            audio.Play();
+        }
+	}
+}
---
+++ Assets/VolumeControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeControl : MonoBehaviour {
+
+    private AudioSource _audioSource;
+    private Slider _slider;
+
+	// Use this for initialization
+	void Start () {
+        _audioSource = GameObject.FindGameObjectWithTag("AudioSource").GetComponent<AudioSource>();
+        _slider = gameObject.GetComponent<Slider>();
+        _slider.value = _audioSource.volume * 100;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        _audioSource.volume = _slider.value / 100;
+	}
+
+
+}
